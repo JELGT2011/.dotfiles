10abfa1d323a0792bca7a55704790371b800c1fc
Jason Libbey <jelgt2011@gmail.com>
2016-05-24 04:54:49 -0400

    add plugins

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+#!/usr/bin/env bash
 
 rcup -x install.sh -x README.md
+
+# vim plugins (don't autorun these, let user choose)
+# yav -u https://github.com/fatih/vim-go
+# yav -u https://github.com/OmniSharp/omnisharp-vim
 
