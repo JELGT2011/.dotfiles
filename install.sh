bc6fee5139d8ec81d074a5fd92e81c50054082e6
Jason Libbey <jelgt2011@gmail.com>
2016-05-24 06:05:54 -0400

    add brew formulas

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-rcup -x install.sh -x README.md
+rcup -x install.sh -x README.md -x osx
 
 # vim plugins (don't autorun these, let user choose)
 # yav -u https://github.com/fatih/vim-go
---
+++ osx/brew-formulas.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+brew install pyenv
+brew install pyenv-virtualenv
+brew install pyenv-virtualenvwrapper
+brew install mono
+
