b73fe4f1664c044cbb554cfff478f49a843dee8c
Jason Libbey <jelgt2011@gmail.com>
2016-05-24 06:18:05 -0400

    added vim-dispatch for omnisharp

--- install.sh
+++ install.sh
@@ -2,7 +2,3 @@
 
 rcup -x install.sh -x README.md -x osx
 
-# vim plugins (don't autorun these, let user choose)
-# yav -u https://github.com/fatih/vim-go
-# yav -u https://github.com/OmniSharp/omnisharp-vim
-
