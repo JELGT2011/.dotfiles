9ced8745fd5d67b25a5c54384abbf6a1380a89b8
Jason Libbey <jelgt2011@gmail.com>
2016-05-25 17:04:02 -0400

    typo

--- zsh.after/ionicsecurity.zsh
+++ zsh.after/ionicsecurity.zsh
@@ -7,6 +7,6 @@ alias dashboard-build-prod="cd /vagrant/client/ && npm run build-pro"
 
 # SSH Addresses
 alias ionic-elastic-search="ssh -L 9200:localhost:9200 ubuntu@52.88.237.76"
-alias ionic-dpxdt="cd ~/ && ssh jlibbey@dpxdt.in.ionisecurity.com"
+alias ionic-dpxdt="cd ~/ && ssh jlibbey@dpxdt.in.ionicsecurity.com"
 alias ionic-dpxdt-export="cd ~/Repositories && scp -r dpxdt_regression_tests/logs jlibbey@dpxdt.in.ionisecurity.com:~/repositories/dpxdt_regression_tests"
 
