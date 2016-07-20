5843c8674f4d0251f7646a114f5d78f58f3e92e8
Jason Libbey <jelgt2011@gmail.com>
2016-07-19 20:32:25 -0700

    remove dotfiles incompatible with uber install scripts

--- zsh.after/pyenv.zsh
+++ zsh.after/pyenv.zsh
@@ -1,8 +1,7 @@
 
 # Use Homebrew's directories rather than ~/.pyenv
-export PYENV_ROOT="/usr/local/var/pyenv"
+# export PYENV_ROOT="/usr/local/var/pyenv"
 
 # Enable shims and autocompletion
-if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
-
+# if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
+# if which pyenv-virtualenv-init > /dev/null; then eval "$(pyenv virtualenv-init -)"; fi
