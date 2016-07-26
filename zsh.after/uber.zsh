a825f6e1b66ca4cd30d099185de07b7c5ed87888
Jason Libbey <jelgt2011@gmail.com>
2016-07-26 15:06:41 -0700

    update uber settings

--- zsh.after/uber.zsh
+++ zsh.after/uber.zsh
@@ -1,3 +1,13 @@
 
-SERVER_NAME="mg-explorer-2"
+# notes
 
+# /usr/local/bin/virtualenvwrapper.sh:50
+# VIRTUALENVWRAPPER_PYTHON="$(command \which python)"
+# changed to
+# VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python"
+
+# gitconfig.user may not be imported correctly on uber's config
+
+export VAGRANTNAME="spyker-journey-4"
+
+alias my-vagrants="boxer list_vagrants --owner='jlibbey@uber.com'"
