83bb9380f37399aa3829e2654bc6604e5db219a1
Jason Libbey <jelgt2011@gmail.com>
2016-07-27 19:44:05 -0700

    update notes

--- zsh.after/uber.zsh
+++ zsh.after/uber.zsh
@@ -5,9 +5,13 @@
 # VIRTUALENVWRAPPER_PYTHON="$(command \which python)"
 # changed to
 # VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python"
-
+#
 # gitconfig.user may not be imported correctly on uber's config
+#
+# login to mysql:
+# mysql -uuber -puber
 
 export VAGRANTNAME="spyker-journey-4"
 
 alias my-vagrants="boxer list_vagrants --owner='jlibbey@uber.com'"
+
