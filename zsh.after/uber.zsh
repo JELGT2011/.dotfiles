c462d4c7617df209da72f0df575a99eab120cbee
Jason Libbey <jelgt2011@gmail.com>
2016-07-28 15:14:22 -0700

    remove old comments

--- zsh.after/uber.zsh
+++ zsh.after/uber.zsh
@@ -1,21 +1,10 @@
 
-# notes
-
-# /usr/local/bin/virtualenvwrapper.sh:50
-# VIRTUALENVWRAPPER_PYTHON="$(command \which python)"
-# changed to
-# VIRTUALENVWRAPPER_PYTHON="/usr/local/bin/python"
-#
-# gitconfig.user may not be imported correctly on uber's config
-#
-# login to mysql:
-# mysql -uuber -puber
-
 export VAGRANTNAME="spyker-journey-4"
 export PROJECTNAME="ufs"
 
 alias uber-list-vagrants="boxer list_vagrants --owner=$UBER_OWNER"
 alias uber-dev-ssh="boxer v $VAGRANTNAME"
+alias uber-dev-sql="mysql -uuber -puber"
 alias uber-adhoc-ssh="boxer v adhoc05-sjc1"
 alias uber-dev="cd $PROJECTNAME && source env/bin/activate"
 alias uber-dev-sync="boxer sync $VAGRANTNAME $PROJECTNAME"
