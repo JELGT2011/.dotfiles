4d3743022049a7a473ce5622cf4434513fa93989
Jason Libbey <jelgt2011@gmail.com>
2016-08-16 16:27:20 -0700

    better aliases

--- zsh.after/uber.zsh
+++ zsh.after/uber.zsh
@@ -1,12 +1,14 @@
 
 export VAGRANTNAME="spyker-journey-4"
 export PROJECTNAME="ufs"
+export ADHOCNAME="adhoc05-sjc1"
 
-alias uber-list-vagrants="boxer list_vagrants --owner=$UBER_OWNER"
-alias uber-dev-ssh="boxer v $VAGRANTNAME"
-alias uber-dev-sql="mysql -uuber -puber"
-alias uber-adhoc-ssh="ssh adhoc05-sjc1"
-alias uber-dev="cd $PROJECTNAME && source env/bin/activate"
+alias uber-dev-vagrants="boxer list_vagrants --owner=$UBER_OWNER"
+alias uber-dev-ssh="ssh uber@$VAGRANTNAME.dev -t 'exec zsh'"
+alias uber-dev-db="mysql -uuber -puber"
+alias uber-dev-activate="source ~/$PROJECTNAME/env/bin/activate"
 alias uber-dev-sync="boxer sync $VAGRANTNAME uber/$PROJECTNAME"
-alias uber-remote-pycharm="ssh -YC4 uber@$VAGRANTNAME.dev charm"
-alias uber-relink="ln -S ~/Uber/sync/$VAGRANTNAME.dev.uber.com/home/uber/ ~/Repositories/"
+alias uber-dev-pycharm="ssh -YC4 uber@$VAGRANTNAME.dev charm"
+alias uber-dev-relink="ln -S ~/Uber/sync/$VAGRANTNAME.dev.uber.com/home/uber/ ~/Repositories/"
+
+alias uber-adhoc-ssh="ssh $ADHOCNAME -t 'exec zsh'"
