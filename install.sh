7661d8fc39114296299ae25ad75d7031de9c7ffc
Jason Libbey <jelgt2011@gmail.com>
2016-08-26 11:20:21 -0700

    move away from bash

--- install.sh
+++ install.sh
@@ -1,36 +1,38 @@
 #!/usr/bin/env bash
 
-here="$(dirname "$0")"
+{
+  # install yadr
+  if [ ! -d "$HOME/.yadr" ]; then
+    sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`"
+  fi
 
-# include common.sh
-. "$here/common.sh"
+  here="$(dirname "$0")"
 
-cd $HOME/.dotfiles
+  # include global.zsh
+  . "$here/zsh.after/global.zsh"
 
-# install yadr
-if [ ! -d "$HOME/.yadr" ]; then
-  sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`"
-fi
+  cd $HOME/.dotfiles
 
-# install rcp
-case "$ostype" in
-  "linux")
-    sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
-    sudo apt-get update
-    sudo apt-get install rcm
-  ;;
-  "osx")
-    brew tap thoughtbot/formulae
-    brew install rcm
-  ;;
-esac
+  # install rcp
+  case "$ostype" in
+    "linux")
+      sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
+      sudo apt-get update
+      sudo apt-get install rcm
+    ;;
+    "osx")
+      brew tap thoughtbot/formulae
+      brew install rcm
+    ;;
+  esac
 
-# install powerline fonts
-if [ ! -d "$HOME/.fonts" ]; then
-  git clone https://github.com/powerline/fonts.git $HOME/.fonts
-  $HOME/.fonts/install.sh
-fi
+  # install powerline fonts
+  if [ ! -d "$HOME/.fonts" ]; then
+    git clone https://github.com/powerline/fonts.git $HOME/.fonts
+    $HOME/.fonts/install.sh
+  fi
 
-dotfilesinstall
+  dotfilesinstall
 
-cd $HOME/.dotfiles
+  cd $HOME/.dotfiles
+}
--- zsh.after/global.zsh
+++ zsh.after/global.zsh
@@ -1,2 +1,34 @@
 
 export DEFAULT_USER="jasonlibbey"
+
+unamestr=$(uname)
+osenums=("linux" "osx" "windows" "unknown")
+ostype=""
+
+exclude=("install.sh" "update.sh" "common.sh" "README.md" "osx")
+
+dotfilesinstall() {
+  local rcmexclude=()
+  for i in $exclude; do
+    rcmexclude+=("-x $i")
+  done
+  rcup $rcmexclude
+  zsh
+}
+
+osdetect() {
+  if [[ "$OSTYPE" == "linux-gnu" ]] ||
+     [[ "$OSTYPE" == "cygwin" ]] ||
+     [[ "$OSTYPE" == "msys" ]]; then
+    ostype="linux"
+  elif [[ "$OSTYPE" == "darwin"* ]] ||
+       [[ "$OSTYPE" == "freebsd"* ]]; then
+    ostype="osx"
+  elif [[ "$OSTYPE" == "win32" ]]; then
+    ostype="windows"
+  else
+    ostype="unknown"
+  fi
+}
+
+osdetect
