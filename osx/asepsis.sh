a4e76e68283cae3f5a53da32cc22c5aa3a201713
Jason Libbey <jelgt2011@gmail.com>
2016-08-31 02:53:03 -0700

    bring asepsis back

---
+++ osx/asepsis.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# csrutil disable; reboot
+# # asepsisctl uninstall_wrapper
+# asepsisctl install
+# csrutil enable; reboot
+# asepsis diagnose
+
+touch ~/.no-asepsis-os-restriction
+touch ~/.asepsis-suppress-update-errors
