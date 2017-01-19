319667be16a85ed556fbc2797cf4994b2a01dfef
Jason Libbey <jlibbey@uber.com>
2017-01-18 16:58:21 -0800

    update openssl env config

--- zsh.after/global.zsh
+++ zsh.after/global.zsh
@@ -1,6 +1,10 @@
 
 export DEFAULT_USER="jlibbey"
 
+export LDFLAGS="-L/usr/local/opt/openssl/lib"
+export CPPFLAGS="-I/usr/local/opt/openssl/include"
+export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
+
 osenums=("linux" "osx" "windows" "unknown")
 ostype=""
 
