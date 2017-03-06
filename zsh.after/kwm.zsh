d926431eeb270dd6bbbfcee1d39531993ea4efd8
Jason Libbey <jlibbey@uber.com>
2017-03-06 14:50:14 -0800

    alias toggle focus follows mouse

--- zsh.after/kwm.zsh
+++ zsh.after/kwm.zsh
@@ -1,2 +1,13 @@
 
+export KWMC_FOCUS_FOLLOWS_MOUSE="on"
+
 alias kwm-restart="brew services restart kwm"
+
+function kwm-toggle-focus() {
+  kwmc config focus-follows-mouse $KWMC_FOCUS_FOLLOWS_MOUSE
+  if [ "$KWMC_FOCUS_FOLLOWS_MOUSE" == "on" ]; then
+    KWMC_FOCUS_FOLLOWS_MOUSE="off"
+  else
+    KWMC_FOCUS_FOLLOWS_MOUSE="on"
+  fi
+}
