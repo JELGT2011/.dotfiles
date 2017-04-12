5fbc2c41deb3f042cae6236faf78fdfec08e91bd
Jason Libbey <jlibbey@uber.com>
2017-04-12 13:19:02 -0700

    update ssh config

--- zsh.after/tmux.zsh
+++ zsh.after/tmux.zsh
@@ -1,9 +1,10 @@
 
+alias tmux="export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock; tmux"
+
 function attach() {
   if [[ "$1" != "" ]]; then
     tmux -CC a -t "$1"
   else
     echo "tmux -CC a -t \$1"
   fi
-
 }
