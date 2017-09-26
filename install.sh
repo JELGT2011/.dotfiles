ec6809dbd4c01b1996f2052c32a204e51c90477d
Jason Libbey <jlibbey@uber.com>
2017-09-25 22:48:51 -0700

    update install

--- install.sh
+++ install.sh
@@ -5,9 +5,10 @@ if [[ ! -d "$HOME/.yadr" ]]; then
   # install rvm, ruby, and rake
   gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
   \curl -sSL https://get.rvm.io | bash
-  source /home/uber/.rvm/scripts/rvm
+  source ~/.rvm/scripts/rvm
   rvm install ruby --latest
   gem install rake
+  alias rake='noglob rake'
   sh -c "`curl -fsSL https://raw.githubusercontent.com/skwp/dotfiles/master/install.sh`"
 fi
 
