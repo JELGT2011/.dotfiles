f3ba307c3f25db317ff9777f5d4dffa1707cf63d
Jason Libbey <jlibbey@uber.com>
2018-02-12 13:09:54 -0800

    don't load all version managers by default

--- zsh.after/envs.zsh
+++ zsh.after/envs.zsh
@@ -1,13 +1,13 @@
 
-eval "$(jenv init -)"
-# conflicts with nvm, which is required at Uber
-# eval "$(nodenv init -)"
-eval "$(pyenv init -)"
-eval "$(rbenv init -)"
+alias loadjenv='eval "$(jenv init -)"'
+alias loadnodenv='eval "$(nodenv init -)"'
+alias loadpyenv='eval "$(pyenv init -)"'
+alias loadrbenv='eval "$(rbenv init -)"'
+
+loadpyenv
 
 alias act="source env/bin/activate"
 
 # use nvm for now
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+alias loadnvm='[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"'
