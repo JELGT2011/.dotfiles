23e39282ba9ec33c6e6adc67d5c61891ba5cb8b1
Jason Libbey <jlibbey@uber.com>
2018-05-09 16:29:32 -0700

    always load envs

--- zsh.after/envs.zsh
+++ zsh.after/envs.zsh
@@ -1,13 +1,12 @@
 
-alias loadjenv='eval "$(jenv init -)"'
-alias loadnodenv='eval "$(nodenv init -)"'
-alias loadpyenv='eval "$(pyenv init -)"'
-alias loadrbenv='eval "$(rbenv init -)"'
-
-loadpyenv
+eval "$(jenv init -)"
+# eval "$(nodenv init -)"
+eval "$(pyenv init -)"
+eval "$(rbenv init -)"
 
 alias act="source env/bin/activate"
 
 # use nvm for now
 export NVM_DIR="$HOME/.nvm"
 alias loadnvm='[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"'
+
