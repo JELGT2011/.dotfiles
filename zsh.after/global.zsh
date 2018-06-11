95be40f38f839f6e889c864c0e8068a0784f3f9d
Jason Libbey <jlibbey@uber.com>
2018-06-10 21:20:25 -0700

    include secret.zsh

--- zsh.after/global.zsh
+++ zsh.after/global.zsh
@@ -1,7 +1,6 @@
 
-export DEFAULT_USER="jasonlibbey"
-
 export LDFLAGS="-L/usr/local/opt/openssl/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl/include"
 export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
-export HOMEBREW_NO_SANDBOX=1
+
+source ~/.secrets.zsh
--- zsh.after/homebrew.zsh
+++ zsh.after/homebrew.zsh
@@ -1,4 +1,6 @@
 
+export HOMEBREW_NO_SANDBOX=1
+
 function brew-upgrade() {
     brew update && brew upgrade
 }
