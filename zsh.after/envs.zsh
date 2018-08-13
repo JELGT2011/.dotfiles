61147c9c21edf5f1b6712c46efd038a8301ca231
Jason Libbey <jlibbey@bird.co>
2018-08-13 16:35:32 -0700

    update configs

--- zsh.after/envs.zsh
+++ zsh.after/envs.zsh
@@ -1,12 +1,7 @@
 
 eval "$(jenv init -)"
-# eval "$(nodenv init -)"
+eval "$(nodenv init -)"
 eval "$(pyenv init -)"
 eval "$(rbenv init -)"
 
 alias act="source env/bin/activate"
-
-# use nvm for now
-export NVM_DIR="$HOME/.nvm"
-alias loadnvm='[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"'
-
