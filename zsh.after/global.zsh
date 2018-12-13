3f0e861d3dd6435bcacdf39573843637df15cc7b
Jason Libbey <jlibbey@bird.co>
2018-12-12 19:49:52 -0800

    remove pkg config

--- zsh.after/global.zsh
+++ zsh.after/global.zsh
@@ -1,7 +1,6 @@
 
 export LDFLAGS="-L/usr/local/opt/openssl/lib"
 export CPPFLAGS="-I/usr/local/opt/openssl/include"
-export PKG_CONFIG_PATH="/usr/local/opt/openssl/lib/pkgconfig"
 
 source ~/.secrets.zsh
 
