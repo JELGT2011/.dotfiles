4d456b8ae5de8ac7934cdbea4011f88748d41011
Jason Libbey <jelgt2011@gmail.com>
2019-06-18 23:24:41 -0700

    update configs

--- zsh.after/global.zsh
+++ zsh.after/global.zsh
@@ -8,6 +8,16 @@ export ANDROID_HOME="/usr/local/share/android-sdk"
 export GRADLE_HOME="/usr/local/bin/gradle"
 export GROOVY_HOME="/usr/local/opt/groovy/libexec"
 
+# give each terminal its own history: https://linux.die.net/man/1/zshoptions
+unsetopt inc_append_history
+unsetopt share_history
+
 function kill-port() {
   kill $(lsof -t -i :${1})
 }
+
+function git-prune-local() {
+  local branches=$(git branch --no-color --format='%(refname:short)')
+}
+
+alias gpl=git-prune-local
