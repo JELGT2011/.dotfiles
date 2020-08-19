cd0f08defde15e0c8e3ff6d7d4d85445bd715fa7
Jason Libbey <jelgt2011@gmail.com>
2020-08-18 23:13:49 -0700

    add android

--- zsh.after/global.zsh
+++ zsh.after/global.zsh
@@ -6,6 +6,11 @@ source "${HOME}/.secrets.zsh"
 export PATH="${PATH}:${HOME}/bin/"
 export PATH="${PATH}:$(go env GOPATH)/bin"
 
+export ANDROID_SDK_ROOT="${HOME}/Library/Android/sdk"
+export ANDROID_HOME="${ANDROID_SDK_ROOT}"
+export PATH="${PATH}:${ANDROID_SDK_ROOT}/tools"
+export PATH="${PATH}:${ANDROID_SDK_ROOT}/platform-tools"
+
 # give each terminal its own history: https://linux.die.net/man/1/zshoptions
 unsetopt inc_append_history
 unsetopt share_history
